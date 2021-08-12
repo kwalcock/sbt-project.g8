abaee60762d8abb1872e9d4f67a311b34e1c4a74
Keith Alcock <github@keithalcock.com>
2021-08-11 22:10:00 -0700

    Compile in subproject

--- src/main/g8/src/main/scala/$package$/apps/HelloWorldApp.scala
+++ src/main/g8/src/main/scala/$package$/apps/HelloWorldApp.scala
@@ -1,9 +1,10 @@
 package $package$.apps
 
+import $package$.$class$
 import $package$.common.utils.$class$App
 
 object HelloWorldApp extends $class$App {
-  val appMessage = args(0).lift.getOrElse(getArgString("apps.HelloWorldApp.message", Some("App message not found!")))
+  val appMessage = args.lift(0).getOrElse(getArgString("apps.HelloWorldApp.message", Some("App message not found!")))
   logger.info(appMessage)
 
   val $class;format="decap"$ = $class$()
