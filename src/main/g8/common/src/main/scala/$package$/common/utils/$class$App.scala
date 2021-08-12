d56dc7ee4a31cdeeb5f0a5361cc1d813ca5bf145
Keith Alcock <github@keithalcock.com>
2021-08-11 21:20:55 -0700

    Exception, App in utils

--- src/main/g8/common/src/main/scala/$package$/common/utils/$class$App.scala
+++ src/main/g8/common/src/main/scala/$package$/common/utils/$class$App.scala
@@ -1,15 +1,13 @@
-package $package$.utils
+package $package$.common.utils
 
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
-import $package$.common.utils.Configured
-import $package$.common.utils.Logging
 
 trait $class$ConfiguredApp extends Configured {
   // This is used instead of load so that no default references or default overrides are involved.
   // In other words, the values you are looking for had better be in this file (resource).
   // This line doesn't work if there is a leading / in the resource name.  I tried.
-  lazy val config = ConfigFactory.parseResourcesAnySyntax("$package$/apps")
+  lazy val config = ConfigFactory.parseResourcesAnySyntax(BuildUtils.pkgToDir("$package$") + "/apps")
 
   def getConf: Config = config
 }
--- src/main/g8/src/main/scala/$package$/apps/HelloWorldApp.scala
+++ src/main/g8/src/main/scala/$package$/apps/HelloWorldApp.scala
@@ -1,6 +1,6 @@
 package $package$.apps
 
-import $package$.utils.$class$App
+import $package$.common.utils.$class$App
 
 object HelloWorldApp extends $class$App {
   val message = getArgString("apps.HelloWorldApp.message", Some("Message not found!"))
